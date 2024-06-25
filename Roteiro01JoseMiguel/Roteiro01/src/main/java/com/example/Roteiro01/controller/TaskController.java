e76247a325652000c25af05e9d7ac9a3fc7458d4
Jose <jmqmagalhaes@sga.pucminas.br>
2024-06-25 19:08:18 -0300

    CrossOrigin correção

--- Roteiro01JoseMiguel/Roteiro01/src/main/java/com/example/Roteiro01/controller/TaskController.java
+++ Roteiro01JoseMiguel/Roteiro01/src/main/java/com/example/Roteiro01/controller/TaskController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/task") // Define o caminho base para todos os endpoints deste controlador
-@CrossOrigin(origins = "https://http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3000/todo")
 public class TaskController {
 
     private final TaskService taskService;
